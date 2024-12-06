3dd50dc092a8fe4c198493dabd225fdf0e88219a
ArinaManaeva <85439563+ArinaManaeva@users.noreply.github.com>
2024-12-07 00:25:44 +0300

    Исправила ошибки

--- LABA1/main.cpp
+++ LABA1/main.cpp
@@ -20,10 +20,13 @@ int wheatEatenByRats = 0;
 int priceOfAcre = 0;
 float ratEatenPercent = 0;
 bool isEndGame = false;
+int acreForPerson = 0;
+int percentOfDeadedRound = 0;
+int percentOfDeaded = 0;
 
 bool Round();
-void GetUserData();
 void Start();
+void EndGame();
 
 
 void SaveMaking()
@@ -47,7 +50,7 @@ void SaveMaking()
 			<< wheatFromAcre << "\n"
 			<< wheatEatenByRats << "\n"
 			<< priceOfAcre << "\n";
-		
+
 		file.close();
 	}
 
@@ -78,13 +81,13 @@ void UploadingSave()
 
 			while (!(cin >> userAnswer))
 			{
-				cin.clear(); 
+				cin.clear();
 				cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 				cout << "Неправильный ввод. Пожалуйста, введите 1 (да) или 0 (нет): " << endl;
 			}
 
-			if (userAnswer == 1) 
+			if (userAnswer == 1)
 			{
 				file.seekg(0, ios::beg);
 
@@ -118,50 +121,119 @@ void UploadingSave()
 }
 
 
-bool Round() 
+bool Round()
 {
 	Start();
 
-	while (numberRound <= 10) 
+	while (numberRound <= 10)
 	{
 
-		priceOfAcre = rand() % 10 + 17; 
+		priceOfAcre = rand() % 10 + 17;
 
 		cout << "Сколько акров земли повелеваешь купить? " << endl;
-		cin >> purchasedAcres;
-		while (purchasedAcres * priceOfAcre > wheat)
+		while (true)
 		{
-			cout << "Не хватает пшеницы для покупки. " << endl << "Сколько акров земли повелеваешь купить? " << endl;
 			cin >> purchasedAcres;
+
+			if (cin.fail() || purchasedAcres < 0)
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Не корректное значение." << endl;
+				cout << "Сколько акров земли повелеваешь купить? " << endl;
+				continue;
+			}
+
+			if (purchasedAcres * priceOfAcre > wheat)
+			{
+				cout << "Не хватает пшеницы для покупки. " << endl;
+				cout << "Сколько акров земли повелеваешь купить? " << endl;
+				cin >> purchasedAcres;
+			}
+			else
+			{
+				break;
+			}
 		}
 		acreLand += purchasedAcres;
 		wheat -= priceOfAcre * purchasedAcres;
 
-		cout << "Сколько акров земли повелеваешь продать?" << endl;
-		cin >> soldedAcres;
-		while (soldedAcres > acreLand) 
+		cout << "Сколько акров земли повелеваешь продать? " << endl;
+		while (true)
 		{
-			cout << "Не хватает земли для продажи. " << endl << "Сколько акров земли повелеваешь продать? " << endl;
 			cin >> soldedAcres;
+
+			if (cin.fail() || soldedAcres < 0)
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Не корректное значение." << endl;
+				cout << "Сколько акров земли повелеваешь продать? " << endl;
+				continue;
+			}
+
+			if (soldedAcres > acreLand)
+			{
+				cout << "Не хватает земли для продажи. " << endl << "Сколько акров земли повелеваешь продать? " << endl;
+				cin >> soldedAcres;
+			}
+			else
+			{
+				break;
+			}
 		}
 		acreLand -= soldedAcres;
 		wheat += priceOfAcre * soldedAcres;
 
 		cout << "Сколько бушелей пшеницы повелеваешь съесть? " << endl;
-		cin >> wheatForEat;
-		while (wheatForEat > wheat)
+		while (true)
 		{
-			cout << "Не хватает пшеницы. " << endl << "Сколько бушелей пшеницы повелеваешь съесть? " << endl;
 			cin >> wheatForEat;
+
+			if (cin.fail() || wheatForEat < 0)
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Не корректное значение." << endl;
+				cout << "Сколько бушелей пшеницы повелеваешь съесть? " << endl;
+				continue;
+			}
+
+			if (wheatForEat > wheat)
+			{
+				cout << "Не хватает пшеницы. " << endl << "Сколько бушелей пшеницы повелеваешь съесть? " << endl;
+				cin >> wheatForEat;
+			}
+			else
+			{
+				break;
+			}
 		}
 		wheat -= wheatForEat;
 
 		cout << "Сколько акров земли повелеваешь засеять? " << endl;
-		cin >> acreForPlanted;
-		while (acreForPlanted > wheat)
+		while (true)
 		{
-			cout << "Не хватает пшеницы. " << endl << "Сколько акров земли повелеваешь засеять? " << endl;
 			cin >> acreForPlanted;
+
+			if (cin.fail() || wheatForEat < 0)
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "Не корректное значение." << endl;
+				cout << "Сколько акров земли повелеваешь засеять? " << endl;
+				continue;
+			}
+
+			if (acreForPlanted > wheat)
+			{
+				cout << "Не хватает пшеницы. " << endl << "Сколько акров земли повелеваешь засеять? " << endl;
+				cin >> acreForPlanted;
+			}
+			else
+			{
+				break;
+			}
 		}
 		wheat -= acreForPlanted;
 
@@ -174,7 +246,7 @@ bool Round()
 
 		// Wheat eaten by rats
 		int wheatBeforeRatEaten = wheat;
-		ratEatenPercent = (rand() % 701) / 10000;
+		ratEatenPercent = (rand() % (7 - 0 + 1) + 0) / 100.0f;
 		//cout << ratEatenPercent << endl;
 		wheat -= (wheat * ratEatenPercent);
 		wheatEatenByRats = wheatBeforeRatEaten - wheat;
@@ -184,6 +256,8 @@ bool Round()
 		//cout << feededPopulation << endl;
 		if (feededPopulation < population) {
 			deadedPeopleOfHunger = population - feededPopulation;
+			percentOfDeadedRound = deadedPeopleOfHunger / population * 100;
+			percentOfDeaded += percentOfDeadedRound;
 			if ((population - deadedPeopleOfHunger) / population < 0.45f) {
 				isEndGame = true;
 			}
@@ -191,14 +265,14 @@ bool Round()
 		}
 
 		// Arrived people
-		arrivedPeople = deadedPeopleOfHunger / 2 + (5 - wheatFromAcre) * wheat / 600 + 1; 
+		arrivedPeople = deadedPeopleOfHunger / 2 + (5 - wheatFromAcre) * wheat / 600 + 1;
 		//cout << arrivedPeople << endl;
 		if (arrivedPeople > 50)
 		{
 			arrivedPeople = 50;
 		}
 
-		else if (arrivedPeople < 0) 
+		else if (arrivedPeople < 0)
 		{
 			arrivedPeople = 0;
 		}
@@ -210,7 +284,7 @@ bool Round()
 			population = floor(population / 2);
 		}
 
-		if (isEndGame) 
+		if (isEndGame)
 		{
 			cout << "Вы проиграли!" << endl;
 			cout << "Номер раунда: " << numberRound << endl;
@@ -236,21 +310,25 @@ bool Round()
 		SaveMaking();
 		Start();
 	}
+	if (numberRound == 10)
+	{
+		EndGame();
+	}
 
 	return 0;
 }
 
 
-void Start() 
+void Start()
 {
 	if (numberRound > 0 && numberRound <= 10) {
 		cout << "Мой повелитель, соизволь поведать тебе. В году " << numberRound << " твоего высочайшего правления " << deadedPeopleOfHunger << " человек умерли с голоду, и "
 			<< arrivedPeople << " человек прибыли в наш великий город; " << endl;
-			if (isPlague) 
-			{
-				cout << "Чума уничтожила половину населения;" << endl;
-			}
-			cout<< "Население города сейчас составляет: " << population << " человек; " << endl <<
+		if (isPlague)
+		{
+			cout << "Чума уничтожила половину населения;" << endl;
+		}
+		cout << "Население города сейчас составляет: " << population << " человек; " << endl <<
 			"Мы собрали " << wheatWasHarvested << " бушелей пшеницы, по " << wheatFromAcre << " бушеля с акра; "
 			<< endl << "Крысы истребили " << wheatEatenByRats << " бушелей пшеницы, оставив " << wheat << " бушеля в амбарах; "
 			<< endl << "Город сейчас занимает " << acreLand << " акров;" << endl <<
@@ -258,14 +336,52 @@ void Start()
 			"Что пожелаешь, повелитель ?" << endl;
 	}
 
-	else 
+	if (numberRound == 10)
+	{
+		cout << "Население: " << population << endl << " Пшеница: " << wheat << endl << " Количество земли: " << acreLand << endl;
+		EndGame();
+	}
+
+	else
 	{
 		cout << "Население: " << population << endl << " Пшеница: " << wheat << endl << " Количество земли: " << acreLand << endl;
 	}
 }
 
 
-int main() 
+void EndGame()
+{
+	cout << "Игра закончена!" << endl;
+	acreForPerson = acreLand / population;
+	percentOfDeaded = percentOfDeaded / 10;
+
+	if (percentOfDeaded > 33 && acreForPerson < 7)
+	{
+		cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города. " << endl <<
+			"Теперь вы вынуждены влачить жалкое существование в изгнании." << endl;
+	}
+
+	if (percentOfDeaded > 10 && acreForPerson < 9)
+	{
+		cout << "Вы правили железной рукой, подобно Нерону и Ивану Грозному. " << endl <<
+			"Народ вздохнул с облегчением, и никто больше не желает видеть вас правителем." << endl;
+	}
+
+	if (percentOfDeaded > 3 && acreForPerson < 10)
+	{
+		cout << "Вы справились вполне неплохо, у вас, конечно, есть недоброжелатели, " << endl <<
+			"но многие хотели бы увидеть вас во главе города снова." << endl;
+	}
+
+	else
+	{
+		cout << "Фантастика! Карл Великий, Дизраэли и Джефферсон вместе не справились бы лучше. " << endl;
+	}
+
+}
+
+
+int main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(static_cast<unsigned int>(time(0)));
